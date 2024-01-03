9b623462d9de2dbf4fa171cbbba9fdd7ef1b4fcd
Dimitri Sifoua <dimitri.sifoua@gmail.com>
2024-01-02 20:38:11 -0500

    Longest substring without repeating characters

--- _0003LongestSubstringWithoutRepeatingCharacters.java
+++ _0003LongestSubstringWithoutRepeatingCharacters.java
@@ -1,12 +1,44 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class _0003LongestSubstringWithoutRepeatingCharacters {
 
-    public int lengthOfLongestSubstring(String s) {
-        // TODO
-        throw new UnsupportedOperationException();
+    public int lengthOfLongestSubstringOptimal(String s) {
+        int maxLength = 0, left = 0, right = 0;
+        Set<Character> memory = new HashSet<>();
+        while(right < s.length()) {
+            char startCharacter = s.charAt(left);
+            char endCharacter = s.charAt(right);
+            if(memory.contains(endCharacter)) {
+                maxLength = Integer.max(maxLength, right - left);
+                // System.out.println(left + " " + right + " " + s.substring(left, right + 1) + " " + maxLength);
+                memory.remove(startCharacter);
+                left += 1;
+                continue;
+            }
+
+            memory.add(endCharacter);
+            right += 1;
+        }
+
+        return Integer.max(maxLength, right - left);
+    }
+
+    public int lengthOfLongestSubstringFastest(String s) {
+        int maxLength = 0, left = 0, right = 0;
+        char currentCharacter;
+        int indexOfFirstOccurrenceOfCurrentCharacter;
+        while(right < s.length()) {
+            currentCharacter = s.charAt(right);
+            indexOfFirstOccurrenceOfCurrentCharacter = s.indexOf(currentCharacter, left);
+            if(indexOfFirstOccurrenceOfCurrentCharacter >= left && indexOfFirstOccurrenceOfCurrentCharacter < right) {
+                // System.out.println(left + " " + right + " " + s.substring(left, right + 1) + " " + maxLength);
+                left = indexOfFirstOccurrenceOfCurrentCharacter + 1;
+            }
+            maxLength = Integer.max(maxLength, right - left + 1);
+            right += 1;
+        }
+
+        return maxLength;
     }
 
     public int lengthOfLongestSubstringBruteForce(String s) {
---
+++ _0004MedianOfTwoSortedArrays.java
@@ -0,0 +1,7 @@
+public class _0004MedianOfTwoSortedArrays {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        return 0d;
+    }
+}
