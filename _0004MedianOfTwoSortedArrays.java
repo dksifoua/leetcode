fc30c9e59f0ce34f5b5ce736cc9a8d5dbbbb578a
Dimitri Sifoua <dimitri.sifoua@gmail.com>
2024-01-04 23:57:09 -0500

    Median of Two Sorted Arrays

--- _0004MedianOfTwoSortedArrays.java
+++ _0004MedianOfTwoSortedArrays.java
@@ -1,21 +1,62 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * <a href="https://leetcode.com/problems/median-of-two-sorted-arrays/">...</a>
+ */
 public class _0004MedianOfTwoSortedArrays {
 
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    /**
+     * Time complexity: O(log(m+n))
+     * Space complexity: O(1)
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArraysOptimal(int[] nums1, int[] nums2) {
+        if(nums1.length > nums2.length) return findMedianSortedArraysOptimal(nums2, nums1);
+
+        int x = nums1.length, y = nums2.length;
+        int low = 0, high = x;
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int minX = partitionX >= 1 ? nums1[partitionX - 1] : Integer.MIN_VALUE;
+            int minY = partitionY >= 1 ? nums2[partitionY - 1] : Integer.MIN_VALUE;
+
+            int maxX = partitionX < nums1.length ? nums1[partitionX] : Integer.MAX_VALUE;
+            int maxY = partitionY < nums2.length ? nums2[partitionY] : Integer.MAX_VALUE;
+
+            if(minX > maxY) high = partitionX - 1;
+            else if(maxX < minY) low = partitionX + 1;
+            else return (x + y) % 2 == 1
+                        ? Integer.max(minX, minY)
+                        : (double) (Integer.max(minX, minY) + Integer.min(maxX, maxY)) / 2
+                        ;
+        }
+
+        throw new IllegalArgumentException("There's no solution for the given inputs!");
+    }
+
+    /**
+     * Time complexity: O(m+n)
+     * Space complexity: O(1)
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArraysBruteForce(int[] nums1, int[] nums2) {
         double median = 0;
-
-        int len1 = nums1.length, len2 = nums2.length;
-        int mid = (len1 + len2) / 2, mod = (len1 + len2) % 2;
-
-        int position1 = mod == 0 ? mid - 1 : mid, position2 = mid;
+        int position2 = (nums1.length + nums2.length) / 2;
+        int position1 = (nums1.length + nums2.length) % 2 == 0 ? position2 - 1 : position2;
 
         int counter = 0, i = 0, j = 0;
         while(counter <= position2) {
-            int num1 = i < len1 ? nums1[i] : Integer.MAX_VALUE;
-            int num2 = j < len2 ? nums2[j] : Integer.MAX_VALUE;
+            int num1 = i < nums1.length ? nums1[i] : Integer.MAX_VALUE;
+            int num2 = j < nums2.length ? nums2[j] : Integer.MAX_VALUE;
 
             if(counter == position1 || counter == position2) median += Integer.min(num1, num2);
 
@@ -25,8 +66,6 @@ public class _0004MedianOfTwoSortedArrays {
             counter += 1;
         }
 
-        median = position1 == position2 ? median : median / 2;
-
-        return median;
+        return position1 == position2 ? median : median / 2;
     }
 }
