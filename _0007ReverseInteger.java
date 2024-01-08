2c5cdfffa81a94c11957f6973be21eb8e994501b
dksifoua <dimitri.sifoua@gmail.com>
2024-01-07 21:21:28 -0500

    Reverse Integer Brute Force

---
+++ _0007ReverseInteger.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class _0007ReverseInteger {
+
+    public static void main(String[] args) {
+        _0007ReverseInteger reverseInteger = new _0007ReverseInteger();
+        int x = 1563847412;
+        int reversedX = reverseInteger.reverse(x);
+        System.out.println(x + " => " + reversedX);
+    }
+
+    public int reverse(int x) {
+        int reversedX = 0;
+        char[] digits = String.valueOf(x).toCharArray();
+        System.out.println(Arrays.toString(digits));
+
+        int left = digits[0] == '-' ? 1 : 0, right = digits.length - 1;
+        while (left < right) {
+            char digit = digits[left];
+            digits[left] = digits[right];
+            digits[right] = digit;
+
+            left += 1; right -= 1;
+        }
+        System.out.println(Arrays.toString(digits));
+
+        int multiplier = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == '-') break;
+            int temp = reversedX;
+            reversedX += (digits[i] - '0') * multiplier;
+            System.out.println(multiplier + " " + reversedX);
+            if ((reversedX - temp) / multiplier != digits[i] - '0' || reversedX < 0) {
+                reversedX = 0;
+                break;
+            }
+            multiplier *= 10;
+        }
+        if (digits[0] == '-') reversedX *= -1;
+
+        return reversedX;
+    }
+}
